128109457571dcc5a9c62a3000acdfcfc8e7a0bc
Eustace Mbanefo <111151661+Phumnanya@users.noreply.github.com>
2025-05-19 14:08:16 +0100

    Update page.js
    removed seo.js

--- app/page.js
+++ app/page.js
@@ -1,12 +1,8 @@
 import Home from "@/app/home/page";
-import SEO from "@/components/SEO";
 
 export default function HomePage() {
   return (
     <>
-      <SEO title="Portfolio Website of Eustace Mbanefo"
-      description="talented Software Engineer open to software projects collaborations and job offers"
-      />
       <Home />
     </>
   );
